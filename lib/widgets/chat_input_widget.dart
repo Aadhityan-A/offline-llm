099c169b59c8c8c07b4da0c617f4ae5f250a06b2
Aadhityan-A <aadhityan1995@gmail.com>
2025-11-27 00:22:00 +0100

    fix: Flutter 3.24.0 compatibility - use withOpacity, add path dep, remove pubspec.lock from tracking

--- lib/widgets/chat_input_widget.dart
+++ lib/widgets/chat_input_widget.dart
@@ -41,7 +41,7 @@ class _ChatInputWidgetState extends State<ChatInputWidget> {
             color: Theme.of(context).colorScheme.surface,
             boxShadow: [
               BoxShadow(
-                color: Colors.black.withValues(alpha: 0.1),
+                color: Colors.black.withOpacity(0.1),
                 blurRadius: 8,
                 offset: const Offset(0, -2),
               ),
